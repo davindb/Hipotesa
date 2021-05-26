ffb4d7bdebcb675007184d51c69126e9b3a2d63c
davindb <davindbhagaspati@gmail.com>
2021-05-26 19:45:13 +0700

    adding pickle sklearn

--- Progress Report/Machine Learning/API/main.py
+++ Progress Report/Machine Learning/API/main.py
@@ -1,24 +1,62 @@
-#pip install fastapi uvicorn
+from flask.helpers import make_response
+from flask.json import jsonify
+import pandas as pd
+from flask import Flask, request, jsonify
+from assets import symptoms_list, disease_description, disease_precaution
+import pickle
 
-# 1. Importing Libraries
-import uvicorn
-from fastapi import FastAPI
+app = Flask(__name__)
+model = open("randomforest.pkl", "rb")
+model = pickle.load(model)
 
-# 2. Create the App object
-app = FastAPI()
-
-# 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/')
+@app.route('/')
 def index():
-    return {'message': 'Hello world!'}
+    return '''
+    Symptoms Based Disease Prediction Public API built by Kreasi Anak Bangsa team from Bangkit 2021.
+    '''
 
-# 4. Route with a single parameter, returns the parameter within a message
-# Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/Home')
-def get_name(name: str):
-    return {'Welcome to Davin Webpage': f'{name}'}
+@app.route('/predict', methods = ['POST'])
+def predict():
+    # 1. Retrieve the data from users as json
+    data = request.get_json()
 
-# 5. Run the API with uvicorn
-# Will run on http://127.0.0.1:8000
+    # 2. Define the inputs for the model
+    model_inputs = []
+    for i in symptoms_list:
+        model_inputs.append(float(0))
+
+    # 3. Create the inputs for the model
+    for i in data.values():
+        if i != '0':
+            symptom_index = symptoms_list.index(i)
+            model_inputs[symptom_index] = float(1)
+
+    # 3. Create the Input DataFrame
+    model_inputs = pd.DataFrame([model_inputs], columns=symptoms_list)
+
+    # 4. Predict the data
+    prediction = model.predict(model_inputs)
+    predicted_disease = prediction[0]
+
+    # 4. Filter the description based on the predicted disease
+    filtered_desc = filter(lambda disease: disease['Disease'] == predicted_disease, disease_description)
+    filtered_desc = list(filtered_desc)[0]['Description']
+
+    # 5. Filter the precaution based on the predicted disease
+    filtered_precaution = filter(lambda disease: disease['Disease'] == predicted_disease, disease_precaution)
+    filtered_precaution = list(filtered_precaution)[0]
+    filtered_precaution = [x[1] for x in filtered_precaution.items() if x[0] != 'Disease' and x[1] != 0]
+
+    # 6. Store the results as json
+    results = {
+        'Disease': predicted_disease,
+        'Description': filtered_desc,
+        'Precaution': filtered_precaution
+    }
+    results = make_response(jsonify(results), 200)
+
+    # 7. Return the results
+    return results
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    app.run()

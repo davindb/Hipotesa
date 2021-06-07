ff756d5a01a700d25231deb8ce538d857a47d51c
davindb <davindbhagaspati@gmail.com>
2021-06-07 13:56:29 +0700

    remove we

--- Machine Learning/TensorFlow API/web/static/local/script.js
+++
@@ -1,9 +0,0 @@
-// import Typed from "typed.js";
-$(document).ready(function () {
-  const typed = new Typed("#typed", {
-    stringsElement: "#typed-strings",
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-  });
-});
